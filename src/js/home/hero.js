7ed25378a363307492b40512bb992877e61c0734
EduardMLT <eddku@i.ua>
2023-07-25 10:25:00 +0300

    initial copy

--- src/js/home/hero.js
+++ src/js/home/hero.js
@@ -1,3 +1,7 @@
+// щоб працював swiper - в index.html внесено
+//<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+//<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
 import swiper from 'swiper';
 
 var  swiper = new Swiper('.swiper', {
