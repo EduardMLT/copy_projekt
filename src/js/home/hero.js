3b878a4cd26362cece644f1eea9ba686c4231b33
EduardMLT <eddku@i.ua>
2023-07-25 11:00:23 +0300

    initial on Swiper

--- src/js/home/hero.js
+++ src/js/home/hero.js
@@ -1,35 +1,35 @@
-import swiper from 'swiper';
+// import swiper from 'swiper';
 
-var  swiper = new Swiper('.swiper', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
+// var  swiper = new Swiper('.swiper', {
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'bullets',
+//         clickable: true,
         
-  },
+//   },
   
-    slidesPerView: 0,
-    centeredSlides: false,
-    slidesPerGroupSkip: 0,
-    grabCursor: true,
-    keyboard: {
-      enabled: true,
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-      },
-    },
+//     slidesPerView: 0,
+//     centeredSlides: false,
+//     slidesPerGroupSkip: 0,
+//     grabCursor: true,
+//     keyboard: {
+//       enabled: true,
+//     },
+//     breakpoints: {
+//       769: {
+//         slidesPerView: 3,
+//         slidesPerGroup: 3,
+//       },
+//     },
   
-     //безкінцево
-    loop : true,    
+//      //безкінцево
+//     loop : true,    
 
-});
+// });
   
 // щоб працював swiper - в index.html внесено
 //<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
